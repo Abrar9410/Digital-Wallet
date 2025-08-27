e97d1c015932cb885c82db65485af249af1f76cd
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-08-28 03:42:19 +0600

    Contact page completed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,8 @@
 const config = {
   baseUrl: import.meta.env.VITE_BASE_URL,
+  emailServiceId: import.meta.env.VITE_EMAIL_SERVICE_ID,
+  emailTemplateId: import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+  emailUserId: import.meta.env.VITE_EMAIL_USER_ID
 };
 
 export default config;
